e7f844315c8bc9c7300201afcd008890b536a59d
Viggo Prehn <v.prehn@campus.tu-berlin.de>
2026-08-12 23:24:22 +0200

    fixed mcfans bug

--- destroyer.js
+++ destroyer.js
@@ -1,20 +1,19 @@
 let todos = document.getElementById("todo_div");
-let parentRect = todos.getBoundingClientRect();
-
-//let left = 100;
-//let right = 1500;
-//let top = 550;
-//let bottom = 900;
+let parentRect = document.getElementById("game").getBoundingClientRect();
 
 // Initial text positions
-let pos = [[640, 200], [720, 200], [760, 200], [800, 200], [840, 200]];
+let pos = [ [parentRect.left + 20, parentRect.top + 280], 
+            [parentRect.left + 20, parentRect.top + 200], 
+            [parentRect.left + 20, parentRect.top + 240], 
+            [parentRect.left + 20, parentRect.top + 120], 
+            [parentRect.left + 20, parentRect.top + 160]];
 
 // Initial movement directions
-let dir = [[2, 1], [2, 2], [4, 1], [2, 0], [0, 1]];
-
-const staticDir = [[2, 1], [2, 2], [4, 1], [2, 0], [0, 1]];
-
-setInterval(moveElements, 10);
+let dir = [ [2, 1],
+            [2, 2], 
+            [4, 1], 
+            [2, 0], 
+            [0, 1]];
 
 function moveElements()
 {
@@ -28,41 +27,38 @@ function moveElements()
     }
 
     // Update positions
-    
     i = 0;
     for (let elem of todos.children)
     {
         pos[i][0] += dir[i][0];
         pos[i][1] += dir[i][1];
 
-        //let outsideBounds =
-        //    pos[i][0] < parentRect.left &&
-        //    pos[i][0] > parentRect.right &&
-        //    pos[i][1] < parentRect.top &&
-        //    pos[i][1] > parentRect.bottom;
+        elem.style.left = pos[i][0] + "px";
+        elem.style.top = pos[i][1] + "px";
 
-        let overlapping =
-            pos[i][0] > 0 &&
-            pos[i][0] < 1300 &&
-            pos[i][1] > 550 &&
-            pos[i][1] < 880;
-        
-        //overlapping = pos[i][0] < 1000;
+        let outsideBounds =
+           elem.getBoundingClientRect().left < parentRect.left ||
+           elem.getBoundingClientRect().right > parentRect.right ||
+           elem.getBoundingClientRect().top < parentRect.top ||
+           elem.getBoundingClientRect().bottom > parentRect.bottom;
 
         // Randomize directions
-        if(Math.random() < 0.01 && overlapping)
+        if(Math.random() < 0.01 && !outsideBounds)
         {
             dir[i][0] = Math.random() * 4;
             dir[i][1] = Math.random() * 4;
         }
-        
-        // Move text back inside bounding box as soon as it leaves
-        if(!overlapping)
+
+        if(outsideBounds)
         {
-            dir[i][0] = ((0 + 1300) / 2.0 - pos[i][0]) / 80.0;
-            dir[i][1] = ((550 + 880) / 2.0 - pos[i][1]) / 80.0;
+            dir[i][0] = -dir[i][0];
+            dir[i][1] = -dir[i][1];
         }
         
         i++;
     }
+
 }
+
+setInterval(moveElements, 10);
+moveElements();
